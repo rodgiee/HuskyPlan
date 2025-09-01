a5f718312edcb3ea49ccdde953a133af1dcce0ee
rodgiee <rebecarodge@gmail.com>
2025-08-31 21:50:34 -0400

    added: react

---
+++ frontend/husky-plan-react/src/App.tsx
@@ -0,0 +1,27 @@
+function App() {
+	
+  return (
+    <>
+    	<header id="header">
+		<h1>HuskyPlan</h1>
+	</header>
+
+	<div id="body">
+		<div id="classbar">
+			<p>classbar</p>
+		</div>
+
+		<div id="calendar">
+			<p>calendar</p>
+		</div>
+	</div>
+
+
+	<footer id="header">
+		<p>This is a student project and is not affiliated with or endorsed by the University of Connecticut.</p>
+	</footer>
+    </>
+  )
+}
+
+export default App
